bafb633254d4efc3c3287811d3d9b3f4b8b09ca9
Tiross <Tiross@users.noreply.github.com>
2015-02-28 01:16:01 +0100

    Formatting months

--- src/format.js
+++ src/format.js
@@ -2,7 +2,36 @@
   'use strict';
 
   if ('function' !== Date.prototype.format) {
+    Date.locale = {};
+    Date.locale.en = {
+      months: {
+        longs: [
+          'January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December'
+        ],
+        shorts: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      days: {
+        longs: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        shorts: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+      },
+      suffixes: ['th', 'st', 'nd', 'rd']
+    };
+
+    Date.locale.use = Date.locale.use || 'en';
+
     Date.prototype.format = function (pattern) {
+      var lpad = function (string, length, pad) {
+        length = typeof length === 'undefined' ? 0 : length;
+        pad = typeof pad === 'undefined' ? '0' : pad;
+
+        while (string.length < length) {
+          string = pad + string;
+        }
+
+        return string;
+      };
+
       var charValues = {
 
         // Year
@@ -23,6 +52,23 @@
         },
         o: function () { // Week year
           return Math.floor(this.getWeekNumber() / 100);
+        },
+
+        // Month
+        n: function () {
+          return this.getMonth() + 1;
+        },
+        m: function () {
+          return lpad(this.format('n'), 2);
+        },
+        M: function () {
+          return Date.locale[ Date.locale.use ].months.shorts[ this.getMonth() ];
+        },
+        F: function () {
+          return Date.locale[ Date.locale.use ].months.longs[ this.getMonth() ];
+        },
+        t: function () {
+          return new Date(this.getFullYear(), this.getMonth() + 1, 0).getDate();
         }
       };
       var that = this;
--- tests/format.js
+++ tests/format.js
@@ -20,3 +20,48 @@ QUnit.test('test years', function (assert) {
   assert.equal(date.format('o'), '2015', '`o` / ISO-8601 year number');
   assert.equal(new Date(2016, 0).format('o'), '2015', '`o` / 1st of 2016 is in the last week of 2015');
 });
+
+QUnit.test('test month', function (assert) {
+  assert.equal(date.format('m'), '01', '`m` / Numeric representation of a month, with leading zeros');
+  assert.equal(date.format('n'), '1', '`n` / Numeric representation of a month, without leading zeros');
+
+  assert.equal(date.format('M'), 'Jan', '`M` / A short textual representation of a month, three letters');
+  assert.equal(new Date(2015, 1).format('M'), 'Feb', '`M` / February');
+  assert.equal(new Date(2015, 2).format('M'), 'Mar', '`M` / March');
+  assert.equal(new Date(2015, 3).format('M'), 'Apr', '`M` / April');
+  assert.equal(new Date(2015, 4).format('M'), 'May', '`M` / May');
+  assert.equal(new Date(2015, 5).format('M'), 'Jun', '`M` / June');
+  assert.equal(new Date(2015, 6).format('M'), 'Jul', '`M` / July');
+  assert.equal(new Date(2015, 7).format('M'), 'Aug', '`M` / August');
+  assert.equal(new Date(2015, 8).format('M'), 'Sep', '`M` / September');
+  assert.equal(new Date(2015, 9).format('M'), 'Oct', '`M` / October');
+  assert.equal(new Date(2015, 10).format('M'), 'Nov', '`M` / November');
+  assert.equal(new Date(2015, 11).format('M'), 'Dec', '`M` / December');
+
+  assert.equal(date.format('F'), 'January', '`F` / A full textual representation of a month, such as January or March');
+  assert.equal(new Date(2015, 1).format('F'), 'February', '`F` / February');
+  assert.equal(new Date(2015, 2).format('F'), 'March', '`F` / March');
+  assert.equal(new Date(2015, 3).format('F'), 'April', '`F` / April');
+  assert.equal(new Date(2015, 4).format('F'), 'May', '`F` / May');
+  assert.equal(new Date(2015, 5).format('F'), 'June', '`F` / June');
+  assert.equal(new Date(2015, 6).format('F'), 'July', '`F` / July');
+  assert.equal(new Date(2015, 7).format('F'), 'August', '`F` / August');
+  assert.equal(new Date(2015, 8).format('F'), 'September', '`F` / September');
+  assert.equal(new Date(2015, 9).format('F'), 'October', '`F` / October');
+  assert.equal(new Date(2015, 10).format('F'), 'November', '`F` / November');
+  assert.equal(new Date(2015, 11).format('F'), 'December', '`F` / December');
+
+  assert.equal(date.format('t'), 31, '`t` / Number of days in the given month');
+  assert.equal(new Date(2015, 1).format('t'), 28, '`t` / February');
+  assert.equal(new Date(2016, 1).format('t'), 29, '`t` / February (leap year)');
+  assert.equal(new Date(2015, 2).format('t'), 31, '`t` / March');
+  assert.equal(new Date(2015, 3).format('t'), 30, '`t` / April');
+  assert.equal(new Date(2015, 4).format('t'), 31, '`t` / May');
+  assert.equal(new Date(2015, 5).format('t'), 30, '`t` / June');
+  assert.equal(new Date(2015, 6).format('t'), 31, '`t` / July');
+  assert.equal(new Date(2015, 7).format('t'), 31, '`t` / August');
+  assert.equal(new Date(2015, 8).format('t'), 30, '`t` / September');
+  assert.equal(new Date(2015, 9).format('t'), 31, '`t` / October');
+  assert.equal(new Date(2015, 10).format('t'), 30, '`t` / November');
+  assert.equal(new Date(2015, 11).format('t'), 31, '`t` / December');
+});
