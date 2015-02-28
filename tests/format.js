971f529ba1a310a1c0524c8af3361aec4e4ea9ec
Tiross <Tiross@users.noreply.github.com>
2015-02-28 01:17:18 +0100

    Small precision on test 'is a leap year'

--- tests/format.js
+++ tests/format.js
@@ -12,10 +12,10 @@ QUnit.test('test years', function (assert) {
   assert.equal(date.format('Y'), year, '`Y` / A full numeric representation of a year, 4 digits');
   assert.equal(date.format('y'), year % 100, '`y` / A two digit representation of a year');
 
-  assert.equal(date.format('L'), '0', '`L` / Whether it\'s a leap year	1 if it is a leap year, 0 otherwise');
-  assert.equal(new Date(1900, 0).format('L'), '0', '`L` / 1900 is not a leap year (1900 % 100 === 0)');
-  assert.equal(new Date(2008, 0).format('L'), '1', '`L` / 2008 is & leap year (2008 % 100 !== 0 && 2008 % 4 === 0)');
-  assert.equal(new Date(2000, 0).format('L'), '1', '`L` / 2000 is & leap year (2000 % 400 === 0)');
+  assert.strictEqual(date.format('L'), '0', '`L` / Whether it\'s a leap year	1 if it is a leap year, 0 otherwise');
+  assert.strictEqual(new Date(1900, 0).format('L'), '0', '`L` / 1900 is not a leap year (1900 % 100 === 0)');
+  assert.strictEqual(new Date(2008, 0).format('L'), '1', '`L` / 2008 is & leap year (2008 % 100 !== 0 && 2008 % 4 === 0)');
+  assert.strictEqual(new Date(2000, 0).format('L'), '1', '`L` / 2000 is & leap year (2000 % 400 === 0)');
 
   assert.equal(date.format('o'), '2015', '`o` / ISO-8601 year number');
   assert.equal(new Date(2016, 0).format('o'), '2015', '`o` / 1st of 2016 is in the last week of 2015');
