7d6e0e404630e9fa0b3728546f8c224bc0f5ec93
Tiross <Tiross@users.noreply.github.com>
2015-03-04 00:18:29 +0100

    Correction for test on Travis

--- spec/formatSpec.js
+++ spec/formatSpec.js
@@ -311,18 +311,18 @@ describe('format spec', function () {
   });
 
   describe('formatting full dates and times', function () {
-    var date = new Date(2015, 0, 1, 11, 30, 12);
+    var date = new Date(Date.UTC(2015, 0, 1, 11, 30, 12));
 
     xit('should return a ISO 8601 representation of date and time with "c"', function () {
-      expect(date.format('c')).toBe('2015-01-01T11:30:12+01:00');
+      expect(date.format('c')).toBe('2015-01-01T12:30:12+01:00');
     });
 
     xit('should return a representation of date and time following RFC 2822 with "r"', function () {
-      expect(date.format('r')).toBe('Thu, 1 Jan 2015 11:30:12 +0100');
+      expect(date.format('r')).toBe('Thu, 1 Jan 2015 12:30:12 +0100');
     });
 
     it('should return the number of seconds since January 1st 1979 with "U"', function () {
-      expect(date.format('U')).toBe('1420108212');
+      expect(date.format('U')).toBe('1420111812');
     });
   });
 
