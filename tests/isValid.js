888e322974440adb7811df3d913e9f011d0b2d6a
Tiross <Tiross@users.noreply.github.com>
2015-03-03 01:05:59 +0100

    qUnit tests removed

--- tests/isValid.js
+++
@@ -1,7 +0,0 @@
-
-QUnit.test('Test method "isValid"', function (assert) {
-  assert.ok(new Date().isValid(), 'new Date() is valid');
-  assert.ok(new Date('2015-01-01').isValid(), 'new Date(\'2015-01-01\') is valid');
-  assert.ok(new Date(2015, 0, 1).isValid(), 'new Date(2015, 0, 1) is valid');
-  assert.ok(!(new Date('lol').isValid()), 'new Date(\'lol\') is invalid');
-});
