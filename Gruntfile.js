2c16ae996012d14592da04106c165e70ecf0745c
Tiross <Tiross@users.noreply.github.com>
2015-03-10 01:21:24 +0100

    Test with "Europe/Paris" if $tz is not defined

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
     karma: {
       options: {
         frameworks: ['jasmine'],
-        files: ['spec/*.js', 'src/*.js', 'spec/env/' + process.env.tz + '.js'],
+        files: ['spec/*.js', 'src/*.js', 'spec/env/' + (process.env.tz ? process.env.tz : 'Europe/Paris') + '.js'],
       },
       unit: {
         browsers: ['PhantomJS', 'Safari', 'Firefox'],
