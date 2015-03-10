b4eb5549a37a56d39f32ec33c28c2cd523ab7d28
Tiross <Tiross@users.noreply.github.com>
2015-03-10 01:22:57 +0100

    Test for UTC

--- spec/env/UTC.js
+++ spec/env/UTC.js
@@ -1,44 +1,44 @@
 describe('format spec', function () {
 
-  xdescribe('formatting timezones', function () {
+  describe('formatting timezones', function () {
 
     it('should return a timezone identifer with "e"', function () {
-      expect(new Date(2015, 0).format('e')).toBe('CET');
+      expect(new Date(2015, 0).format('e')).toBe('UTC');
     });
 
     it('should return 1 or 0 when given date is in DST or not with "I"', function () {
       expect(new Date(2015, 0).format('I')).toBe('0');
-      expect(new Date(2015, 3).format('I')).toBe('1'); // April 1st is in DST in France
+      expect(new Date(2015, 3).format('I')).toBe('O'); // no DST in UTC
     });
 
     it('should return the difference to Greenwich time without colon with "O"', function () {
       var date = new Date(2015, 0);
 
-      expect(date.format('O')).toBe(date.format('I') === '1' ? '+0200' : '+0100');
+      expect(date.format('O')).toBe('+0000');
     });
 
     it('should return the difference to Greenwich time with colon with "P"', function () {
       var date = new Date(2015, 0);
 
-      expect(date.format('P')).toBe(date.format('I') === '1' ? '+02:00' : '+01:00');
+      expect(date.format('P')).toBe('+00:00');
     });
 
     it('should return the timezone abbreviation with "T"', function () {
-      expect(new Date(2015, 0).format('T')).toBe('CET');
+      expect(new Date(2015, 0).format('T')).toBe('UTC');
     });
 
     it('should return the timezone offset is seconds with "Z"', function () {
       var date = new Date(2015, 0);
 
-      expect(date.format('Z')).toBe(date.format('I') === '1' ? '7200' : '3600');
+      expect(date.format('Z')).toBe('0');
     });
   });
 
   describe('formatting full dates and times', function () {
     var date = new Date(Date.UTC(2015, 0, 1, 11, 30, 12));
 
-    xit('should return a ISO 8601 representation of date and time with "c"', function () {
-      expect(date.format('c')).toBe('2015-01-01T12:30:12+01:00');
+    it('should return a ISO 8601 representation of date and time with "c"', function () {
+      expect(date.format('c')).toBe('2015-01-01T11:30:12+00:00');
     });
 
     it('should return a representation of date and time following RFC 2822 with "r"', function () {
@@ -49,7 +49,7 @@ describe('format spec', function () {
   describe('formatting with composed pattern', function () {
     var date = new Date(2015, 0, 1, 11, 30, 12);
 
-    xit('should return "le 01/01/2015 à 11:30:12"', function () {
+    it('should return "le 01/01/2015 à 11:30:12"', function () {
       expect(date.format('\\l\\e d/m/Y \\à H:i:s')).toBe('le 01/01/2015 à 11:30:12');
     });
   });
