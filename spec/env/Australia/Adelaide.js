bae990fb0dc55312108095c5bed19bb374afa382
Tiross <Tiross@users.noreply.github.com>
2015-03-10 23:26:39 +0100

    Correction on Adelaide specs

--- spec/env/Australia/Adelaide.js
+++ spec/env/Australia/Adelaide.js
@@ -17,20 +17,20 @@ describe('format spec', function () {
     });
 
     it('should return 1 or 0 when given date is in DST or not with "I"', function () {
-      expect(new Date(2015, 0).format('I')).toBe('0');
-      expect(new Date(2015, 6).format('I')).toBe('1'); // July 1st is in DST
+      expect(new Date(2015, 0).format('I')).toBe('1'); // July 1st is in DST
+      expect(new Date(2015, 6).format('I')).toBe('0'); // I forgot it's in south hemisphere
     });
 
     it('should return the difference to Greenwich time without colon with "O"', function () {
       var date = new Date(2015, 0);
 
-      expect(date.format('O')).toBe(date.format('I') === '1' ? '-1030' : '-0930');
+      expect(date.format('O')).toBe(date.format('I') === '1' ? '1030' : '0930');
     });
 
     it('should return the difference to Greenwich time with colon with "P"', function () {
       var date = new Date(2015, 0);
 
-      expect(date.format('P')).toBe(date.format('I') === '1' ? '-10:30' : '-09:30');
+      expect(date.format('P')).toBe(date.format('I') === '1' ? '10:30' : '09:30');
     });
 
     it('should return the timezone abbreviation with "T"', function () {
@@ -40,7 +40,7 @@ describe('format spec', function () {
     it('should return the timezone offset is seconds with "Z"', function () {
       var date = new Date(2015, 0);
 
-      expect(date.format('Z')).toBe(date.format('I') === '1' ? '-37800' : '-34200');
+      expect(date.format('Z')).toBe(date.format('I') === '1' ? '37800' : '34200');
     });
   });
 
@@ -48,11 +48,11 @@ describe('format spec', function () {
     var date = new Date(Date.UTC(2015, 0, 1, 11, 30, 12));
 
     it('should return a ISO 8601 representation of date and time with "c"', function () {
-      expect(date.format('c')).toBe('2015-01-01T12:30:12+01:00');
+      expect(date.format('c')).toBe('2015-01-01T22:00:12+10:30');
     });
 
     it('should return a representation of date and time following RFC 2822 with "r"', function () {
-      expect(date.format('r')).toBe('Thu, 1 Jan 2015 12:30:12 +0100');
+      expect(date.format('r')).toBe('Thu, 1 Jan 2015 22:00:12 +1030');
     });
   });
 
